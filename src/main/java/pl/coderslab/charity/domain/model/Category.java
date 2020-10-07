c6c158df008cfca43ba66082fdb1f19f52a504ca
Akjos <sojka_p@o2.pl>
2020-10-07 19:31:47 +0200

    create (entity, repository, service) Category

---
+++ src/main/java/pl/coderslab/charity/domain/model/Category.java
@@ -0,0 +1,24 @@
+package pl.coderslab.charity.domain.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = "id")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
