d0354e4980d21041a2ddaaa857b037c1788c89e7
Akjos <sojka_p@o2.pl>
2020-10-07 22:12:33 +0200

    show institutionList in index

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,15 +1,21 @@
 package pl.coderslab.charity;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.institution.InstitutionService;
 
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final InstitutionService institutionService;
 
     @RequestMapping("/")
-    public String homeAction(){
+    public String homeAction(Model model){
+        model.addAttribute("institutionList", institutionService.getInstitutionList());
         return "/index";
     }
 }
--- src/main/java/pl/coderslab/charity/institution/InstitutionService.java
+++ src/main/java/pl/coderslab/charity/institution/InstitutionService.java
@@ -1,7 +1,22 @@
 package pl.coderslab.charity.institution;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.domain.model.Institution;
+import pl.coderslab.charity.domain.repositories.InstitutionRepository;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class InstitutionService {
+
+    private final InstitutionRepository institutionRepository;
+
+    public List<Institution> getInstitutionList() {
+        log.debug("Get all institutions");
+        return institutionRepository.findAll();
+    }
 }
