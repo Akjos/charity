f941be1dd18b93fe46bea6651936fb7b5f117fe9
Akjos <sojka_p@o2.pl>
2020-10-08 19:21:17 +0200

    load category from database and show list in add donation form

--- src/main/java/pl/coderslab/charity/category/CategoryService.java
+++ src/main/java/pl/coderslab/charity/category/CategoryService.java
@@ -1,7 +1,19 @@
 package pl.coderslab.charity.category;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.domain.model.Category;
+import pl.coderslab.charity.domain.repositories.CategoryRepository;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> getCategoryList() {
+        return categoryRepository.findAll();
+    }
 }
--- src/main/java/pl/coderslab/charity/donation/DonationController.java
+++ src/main/java/pl/coderslab/charity/donation/DonationController.java
@@ -5,15 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.category.CategoryService;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/donation")
 public class DonationController {
     private final DonationService donationService;
+    private final CategoryService categoryService;
 
     @GetMapping("/add")
     public String prepareFormToAddDonation(Model model) {
+        model.addAttribute("categoryList", categoryService.getCategoryList());
         return "/form";
     }
 }
