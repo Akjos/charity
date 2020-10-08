e1da732d3e560bbd9512726d54c94c8095fee753
Akjos <sojka_p@o2.pl>
2020-10-08 21:42:41 +0200

    show institution list in add donationForm, send form to post in controller, fix show image icon in footer

--- src/main/java/pl/coderslab/charity/donation/DonationAddFormDTO.java
+++ src/main/java/pl/coderslab/charity/donation/DonationAddFormDTO.java
@@ -1,6 +1,7 @@
 package pl.coderslab.charity.donation;
 
 import lombok.Data;
+import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
@@ -8,11 +9,12 @@ import java.time.LocalTime;
 import java.util.List;
 
 @Data
+@ToString
 public class DonationAddFormDTO {
 
     private List<Integer> categories;
 
-    private Integer institutionId;
+    private Integer organization;
 
     private String zipCode;
 
@@ -23,6 +25,8 @@ public class DonationAddFormDTO {
     private Integer Quantity;
 
     private String pickUpComment;
+
+    private String phone;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate pickUpDate;
--- src/main/java/pl/coderslab/charity/donation/DonationController.java
+++ src/main/java/pl/coderslab/charity/donation/DonationController.java
@@ -1,23 +1,36 @@
 package pl.coderslab.charity.donation;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.category.CategoryService;
+import pl.coderslab.charity.institution.InstitutionService;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/donation")
+@Slf4j
 public class DonationController {
     private final DonationService donationService;
     private final CategoryService categoryService;
+    private final InstitutionService institutionService;
 
     @GetMapping("/add")
     public String prepareFormToAddDonation(Model model) {
         model.addAttribute("categoryList", categoryService.getCategoryList());
+        model.addAttribute("institutionList", institutionService.getInstitutionList());
         model.addAttribute("donation", new DonationAddFormDTO());
         return "/form";
+    }
+
+    @PostMapping("/add")
+    public String addDonation(@ModelAttribute("donation") DonationAddFormDTO donationDTO) {
+        log.warn("Donation add to database: {} ",donationDTO);
+        return "redirect:/donation/add";
     }
 }
