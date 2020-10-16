c0c4150eb1838d0c952fc32f7cd0765907c464fc
Akjos <sojka_p@o2.pl>
2020-10-16 16:32:22 +0200

    add two admins when program start

--- src/main/java/pl/coderslab/charity/SetStartingData.java
+++ src/main/java/pl/coderslab/charity/SetStartingData.java
@@ -4,10 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import pl.coderslab.charity.domain.model.Role;
+import pl.coderslab.charity.domain.model.User;
 import pl.coderslab.charity.domain.repositories.RoleRepository;
+import pl.coderslab.charity.domain.repositories.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -16,11 +20,14 @@ import java.util.List;
 public class SetStartingData implements ApplicationRunner {
 
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         List<Role> roles = roleRepository.findAll();
-        if(roles.isEmpty()) {
+        if (roles.isEmpty()) {
             Role adminRole = new Role();
             adminRole.setName("ROLE_ADMIN");
             roles.add(adminRole);
@@ -29,6 +36,25 @@ public class SetStartingData implements ApplicationRunner {
             userRole.setName("ROLE_USER");
             roles.add(userRole);
             roleRepository.saveAll(roles);
+        }
+
+        if (userRepository.findAllActiveUsers("ROLE_ADMIN").isEmpty()) {
+            List<User> users = new ArrayList<>();
+            User user1 = new User();
+            user1.setUsername("admin1");
+            user1.setActive(true);
+            user1.setPassword(passwordEncoder.encode("pass"));
+            user1.setRole(roleRepository.findByName("ROLE_ADMIN"));
+            users.add(user1);
+
+            User user2 = new User();
+            user2.setUsername("admin2");
+            user2.setActive(true);
+            user2.setPassword(passwordEncoder.encode("pass"));
+            user2.setRole(roleRepository.findByName("ROLE_ADMIN"));
+            users.add(user2);
+
+            userRepository.saveAll(users);
         }
     }
 }
