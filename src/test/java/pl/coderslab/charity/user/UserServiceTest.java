20b14f9187a4e039ba80f871b7e241128dccb489
Akjos <sojka_p@o2.pl>
2020-11-01 15:30:05 +0100

    add part test save admin

--- src/test/java/pl/coderslab/charity/user/UserServiceTest.java
+++ src/test/java/pl/coderslab/charity/user/UserServiceTest.java
@@ -15,7 +15,10 @@ import pl.coderslab.charity.domain.model.Role;
 import pl.coderslab.charity.domain.model.User;
 import pl.coderslab.charity.domain.repositories.RoleRepository;
 import pl.coderslab.charity.domain.repositories.UserRepository;
+import pl.coderslab.charity.rest.InvalidDataException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
@@ -32,6 +35,7 @@ public class UserServiceTest {
     private Role adminRole;
     private String userRoleName = "ROLE_USER";
     private String adminRoleName = "ROLE_ADMIN";
+    private String userName = "UserName";
     private Long userId = 4L;
 
     @Spy
@@ -48,7 +52,7 @@ public class UserServiceTest {
 
     @Before
     public void setUser() {
-        user = User.builder().id(userId).build();
+        user = User.builder().id(userId).username(userName).role(null).build();
         userRole = Role.builder().id(1L).name(userRoleName).build();
         adminRole = Role.builder().id(2L).name(adminRoleName).build();
     }
@@ -108,4 +112,89 @@ public class UserServiceTest {
         assertTrue(passwordEncoder.matches(userRegisterDTO.getPassword(), argumentCaptor.getValue().getPassword()));
     }
 
+    @Test
+    public void shouldReturnAllAdminUserViewDTO() {
+//        given
+        List<UserViewDTO> list = new ArrayList<>();
+        when(userRepositoryMock.findAllActiveUsers(anyString())).thenReturn(list);
+
+//        when
+        List<UserViewDTO> returnList = testObject.getAllAdmin();
+
+//        then
+        assertNotNull(returnList);
+    }
+
+    @Test
+    public void shouldUseRoleNameToGetAllAdmin() {
+//        given
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        when(userRepositoryMock.findAllActiveUsers(anyString())).thenReturn(anyList());
+
+//        when
+        testObject.getAllAdmin();
+
+//        then
+        verify(userRepositoryMock).findAllActiveUsers(argumentCaptor.capture());
+        assertEquals(argumentCaptor.getValue(), adminRoleName);
+    }
+
+    @Test(expected = InvalidDataException.class)
+    public void shouldTrowInvalidDataExceptionWhenUserExistInDb() {
+//        given
+        UserRegisterDTO userRegisterDTO = UserRegisterDTO.builder()
+                .username("userName")
+                .build();
+        when(userRepositoryMock.checkIfUserExistByUsername(anyString())).thenReturn(true);
+
+//        when
+        testObject.saveAdmin(userRegisterDTO);
+
+//        then
+    }
+
+    @Test
+    public void shouldSetDataToSaveAdmin() {
+//        given
+        UserRegisterDTO userDTO = UserRegisterDTO
+                .builder()
+                .username(userName)
+                .password("password")
+                .build();
+        user.setRole(adminRole);
+        when(roleRepositoryMock.findByName(adminRoleName)).thenReturn(adminRole);
+        when(userRepositoryMock.checkIfUserExistByUsername(anyString())).thenReturn(false);
+        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
+        when(userRepositoryMock.save(any(User.class))).thenReturn(user);
+
+
+//        when
+        testObject.saveAdmin(userDTO);
+
+//        then
+        verify(userRepositoryMock).save(argumentCaptor.capture());
+        assertEquals(adminRole, argumentCaptor.getValue().getRole());
+        assertEquals(argumentCaptor.getValue().getUsername(), userDTO.getUsername());
+        assertTrue(argumentCaptor.getValue().isActive());
+    }
+
+    @Test
+    public void shouldEncodePasswordForAdmin() {
+        //        given
+        ArgumentCaptor<User> argumentCaptor = ArgumentCaptor.forClass(User.class);
+        UserRegisterDTO userRegisterDTO = UserRegisterDTO.builder()
+                .password("password")
+                .build();
+        user.setRole(adminRole);
+        when(roleRepositoryMock.findByName(adminRoleName)).thenReturn(adminRole);
+        when(userRepositoryMock.save(any(User.class))).thenReturn(user);
+
+//        when
+        testObject.saveAdmin(userRegisterDTO);
+
+
+//        then
+        verify(userRepositoryMock).save(argumentCaptor.capture());
+        assertTrue(passwordEncoder.matches(userRegisterDTO.getPassword(), argumentCaptor.getValue().getPassword()));
+    }
 }
